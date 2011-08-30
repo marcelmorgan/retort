bb9337db18613a75c7112a000977af98e97522fb
Marcel Morgan <marcel.morgan@gmail.com>
2011-08-29 23:04:11 -0500

    Convert tabs to spaces for better github viewing

--- lib/retort/core_ext.rb
+++ lib/retort/core_ext.rb
@@ -1,29 +1,29 @@
 class Fixnum
-	def bytes
-		self
-	end
+  def bytes
+    self
+  end
 
-	def kilobytes
-		bytes / 1024.0
-	end
+  def kilobytes
+    bytes / 1024.0
+  end
 
-	def kbs
-		"#{sprintf('%.2f', kilobytes)}KB/s"
-	end
+  def kbs
+    "#{sprintf('%.2f', kilobytes)}KB/s"
+  end
 
-	def megabytes
-		kilobytes / 1024.0
-	end
+  def megabytes
+    kilobytes / 1024.0
+  end
 
 end
 
 class Float
-	def percent
-		(self * 100).to_i
-	end	
+  def percent
+    (self * 100).to_i
+  end 
 
-	def fmt
-		"#{sprintf('%.2f', self)}"
-	end
+  def fmt
+    "#{sprintf('%.2f', self)}"
+  end
 end
 
